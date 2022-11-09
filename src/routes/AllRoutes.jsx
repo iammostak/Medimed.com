82a3a134167a0178580cc6bc7abdbc92f9c7aa21
pratik kharat <pratik29march@gmail.com>
2022-11-09 22:36:17 +0530

    Wellness page added

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,14 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./Home/Home";
 import Login from "./Login/Login";
-import Pricing from "./Pricing/Pricing";
+import Welness from "./Welness/Welness"
 import Signup from "./Signup/Signup";
 
 function AllRoutes() {
    return (
       <Routes>
          <Route path="/" element={<Home />} />
-         <Route path="/pricing" element={<Pricing />} />
+         <Route path="/welness" element={<Welness />} />
          <Route path="/login" element={<Login />} />
          <Route path="/signup" element={<Signup />} />
       </Routes>
