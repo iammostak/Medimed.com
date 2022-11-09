6e18c1b4884fc626e9f98969d587155d1a84145e
pratik kharat <pratik29march@gmail.com>
2022-11-09 22:43:32 +0530

    welness page redone

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -1,16 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./Home/Home";
 import Login from "./Login/Login";
-import Welness from "./Welness/Welness"
+import Pricing from "./Pricing/Pricing";
 import Signup from "./Signup/Signup";
+import Welness from "./Welness/Welness";
 
 function AllRoutes() {
    return (
       <Routes>
          <Route path="/" element={<Home />} />
-         <Route path="/welness" element={<Welness />} />
+         <Route path="/pricing" element={<Pricing />} />
          <Route path="/login" element={<Login />} />
          <Route path="/signup" element={<Signup />} />
+         <Route path="/welness" element={<Welness/>}/>
       </Routes>
    );
 }
