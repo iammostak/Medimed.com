510921d7bd8fe38a0ccf27755a82d50124434355
SK ISMILE <101392872+skismile@users.noreply.github.com>
2022-11-12 16:33:51 +0530

    add payment data

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,6 @@ import { combineReducers, applyMiddleware, legacy_createStore } from "redux";
 import thunk from "redux-thunk";
 import { AuthReducer } from "./MainAuth/AuthReducer";
 const rootReducer = combineReducers({
-    auth: AuthReducer
+    auth: AuthReducer 
 })
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
