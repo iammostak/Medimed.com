07919ed23cd04b3f683e41160435b2664ea04b27
SK ISMILE <101392872+skismile@users.noreply.github.com>
2022-11-12 22:30:49 +0530

    interceptor added link local host change and all functionality added

--- src/routes/Cart/Cart.jsx
+++ src/routes/Cart/Cart.jsx
@@ -1,4 +1,12 @@
-import { Box, Button, Flex, Heading, Skeleton, Text, useToast } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Skeleton,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
@@ -6,119 +14,172 @@ import { Link } from "react-router-dom";
 import CartCard from "./CartCard";
 import PaymentDetails from "./paymentDetails/PaymentDetails";
 const mainColor = "rgb(50,174,177)";
-const getCartData = async () => {
+const getCartData = async (id) => {
   // https://medimedcom-backend-production.up.railway.app/products
-  let data = await axios.get("http://localhost:8080/carts", {
-    headers: { userid: "636d1da8f6cde62d954b2473" },
-  });
+  let data = await axios.get(
+    "https://medimedcom-backend-production.up.railway.app/carts",
+    {
+      headers: { userid: id },
+    }
+  );
   return data;
 };
 function Cart() {
-    const[cartData,setCartData]=useState([])
-    const toast=useToast()
-    const [cartState,setCartState]=useState({
-        loading:false,
-        error:false,
-        success:false
-    })
-// const payment=useSelector(store=>store.paymentState
-// )
-// console.log("payment",payment)
+  const userData = useSelector((store) => store.auth);
+  const [cartData, setCartData] = useState([]);
+  const toast = useToast();
+  const {
+    data: { _id },
+  } = userData;
+  const [cartState, setCartState] = useState({
+    loading: false,
+    error: false,
+    success: false,
+  });
+
   const TotalPrice = cartData?.reduce(
-    (acc, el) =>
-      acc +
-      Number(
-        el.productId.price
-      ) *
-        el.quantity,
-    0
-    )
-    const finalePrice = TotalPrice.toFixed();
-  const FullPrice = cartData.reduce(
-    (acc, el) =>
-      acc +
-      Number(
-        el.productId.Fprice
-      ) *
-        el.quantity,
+    (acc, el) => acc + Number(el.productId.price) * el.quantity,
     0
   );
-  const fullMrp = FullPrice.toFixed(2);
-const handleQty=async({id,type})=>{
-    setCartState({...cartState,loading:true,error:false,success:false})
-try{
-    // let d=await axios.post("https://medimedcom-backend-production.up.railway.app/carts/update",{
-    let d=await axios.post("http://localhost:8080/carts/update",{
-        type:type,
-        productId:id._id
-    })
-}catch(e){
-    console.log(e)
-    setCartState({...cartState,loading:false,error:true,success:false})
-}
-getCartData()
-.then((res) => {
-  setCartData([...res.data])
-  // console.log(res.data);
-})
-.catch((e) => {
-  // console.log(e);
-})
-setCartState({...cartState,loading:false,error:false,success:true})
-}
-const removeCartHandler=async(id)=>{
-    setCartState({...cartState,loading:true,error:false,success:false})
-try{
-    await axios.post("http://localhost:8080/carts/remove",{
-        productId:id
-    })
-    getCartData()
-    .then((res) => {
-        setCartData([...res.data])
-        // console.log(res.data);
-    })
-    .catch((e) => {
-        // console.log(e);
-    })
-    toast({
-        title: `item removed succesfully`,
-        status: "info",
-        isClosable: true,
-      })
-      setCartState({...cartState,loading:false,error:false,success:true})
-}catch(e){
-    toast({
-        title: `error is ${e.message}`,
-        status: "info",
-        isClosable: true,
-      })
-    setCartState({...cartState,loading:false,error:true,success:false})
-}
-}
+  const finalePrice = TotalPrice.toFixed();
+  const FullPrice = cartData.reduce(
+    (acc, el) => acc + Number(el.productId.Fprice) * el.quantity,
+    0
+  );
+
   useEffect(() => {
- setCartState({...cartState,loading:true,success:false,error:false})
-    getCartData()
+    getCartData(_id)
       .then((res) => {
-        setCartData([...res.data])
+        setCartData([...res.data]);
         // console.log(res.data);
-        setCartState({...cartState,loading:false,success:true,error:false})
       })
       .catch((e) => {
         console.log(e);
-        setCartState({...cartState,loading:false,success:false,error:true})
-      })
-    }, []);
-    // for if empty cart
-    if(cartData.length===0)
-    {
-        return<Flex h="350px" flexDirection={"column"} alignItems="center" >
-       <Box h={"300px"}  w={"100%"} backgroundRepeat={"no-repeat"} backgroundPosition="center" backgroundSize={"contain"} backgroundImage={"url(https://user-images.githubusercontent.com/101392872/201416025-b7d96214-f2e4-43db-9a66-ae4002d5a793.png)"}></Box>
-<br /><br />
-<Link to="/"  ><Button bg={mainColor} color="white" colorScheme={"none"} >Back To Home page</Button></Link>
-        </Flex>
+      });
+  }, [_id]);
+
+  const fullMrp = FullPrice.toFixed(2);
+  const handleQty = async ({ id, type }) => {
+    setCartState({ ...cartState, loading: true, error: false, success: false });
+    try {
+      // let d=await axios.post("https://medimedcom-backend-production.up.railway.app/carts/update",{
+      let d = await axios.post(
+        "https://medimedcom-backend-production.up.railway.app/carts/update",
+        {
+          type: type,
+          productId: id._id,
+        }
+      );
+    } catch (e) {
+      console.log(e);
+      setCartState({
+        ...cartState,
+        loading: false,
+        error: true,
+        success: false,
+      });
     }
-    //else part
+    getCartData(_id)
+      .then((res) => {
+        setCartData([...res.data]);
+        // console.log(res.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+    setCartState({ ...cartState, loading: false, error: false, success: true });
+  };
+  const removeCartHandler = async (id) => {
+    setCartState({ ...cartState, loading: true, error: false, success: false });
+    try {
+      await axios.post(
+        "https://medimedcom-backend-production.up.railway.app/carts/remove",
+        {
+          productId: id,
+        }
+      );
+      getCartData(_id)
+        .then((res) => {
+          setCartData([...res.data]);
+          // console.log(res.data);
+        })
+        .catch((e) => {
+          // console.log(e);
+        });
+      toast({
+        title: `item removed succesfully`,
+        status: "info",
+        isClosable: true,
+      });
+      setCartState({
+        ...cartState,
+        loading: false,
+        error: false,
+        success: true,
+      });
+    } catch (e) {
+      toast({
+        title: `error is ${e.message}`,
+        status: "info",
+        isClosable: true,
+      });
+      setCartState({
+        ...cartState,
+        loading: false,
+        error: true,
+        success: false,
+      });
+    }
+  };
+  useEffect(() => {
+    setCartState({ ...cartState, loading: true, success: false, error: false });
+    getCartData()
+      .then((res) => {
+        setCartData([...res.data]);
+        // console.log(res.data);
+        setCartState({
+          ...cartState,
+          loading: false,
+          success: true,
+          error: false,
+        });
+      })
+      .catch((e) => {
+        console.log(e);
+        setCartState({
+          ...cartState,
+          loading: false,
+          success: false,
+          error: true,
+        });
+      });
+  }, []);
+  // for if empty cart
+  if (cartData.length === 0) {
     return (
+      <Flex h="350px" flexDirection={"column"} alignItems="center">
+        <Box
+          h={"300px"}
+          w={"100%"}
+          backgroundRepeat={"no-repeat"}
+          backgroundPosition="center"
+          backgroundSize={"contain"}
+          backgroundImage={
+            "url(https://user-images.githubusercontent.com/101392872/201416025-b7d96214-f2e4-43db-9a66-ae4002d5a793.png)"
+          }
+        ></Box>
+        <br />
+        <br />
+        <Link to="/">
+          <Button bg={mainColor} color="white" colorScheme={"none"}>
+            Back To Home page
+          </Button>
+        </Link>
+      </Flex>
+    );
+  }
+  //else part
+  return (
     <Box
       display={["blok", "blok", "blok", "flex", "flex"]}
       gap={8}
@@ -126,8 +187,14 @@ try{
       margin="auto"
       marginTop={10}
     >
-      {cartData&&<PaymentDetails price={fullMrp} total={finalePrice} cartState={cartState}   />}
-      <Skeleton isLoaded={!cartState.loading} w="100%" >
+      {cartData && (
+        <PaymentDetails
+          price={fullMrp}
+          total={finalePrice}
+          cartState={cartState}
+        />
+      )}
+      <Skeleton isLoaded={!cartState.loading} w="100%">
         <Heading marginBottom={10}>Order Summary</Heading>
         <Text>PRODUCTS</Text>
         {cartData.length !== 0 ? (
--- src/routes/Wellness/SingleProductPage/reviewModal.jsx
+++ src/routes/Wellness/SingleProductPage/reviewModal.jsx
@@ -5,63 +5,85 @@ import {
   ModalHeader,
   ModalFooter,
   ModalBody,
-  ModalCloseButton,Button, Input, Textarea, useToast,
-} from '@chakra-ui/react'
-import { useDisclosure } from "@chakra-ui/react"
-import { useState } from 'react';
-import Rating from './rating/Rating'
+  ModalCloseButton,
+  Button,
+  Input,
+  Textarea,
+  useToast,
+} from "@chakra-ui/react";
+import { useDisclosure } from "@chakra-ui/react";
+import { useState } from "react";
+import Rating from "./rating/Rating";
 const mainColor = "rgb(50,174,177)";
 export default function ReviewModal() {
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    const[text,setText]=useState({
-      name:"",
-      message:""
-    })
-    const toast = useToast()
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [text, setText] = useState({
+    name: "",
+    message: "",
+  });
+  const toast = useToast();
 
-    return (
-      <>
-        <Button onClick={onOpen} bg={mainColor} color="white" fontSize={"10px"}  >WRITE REVIEW</Button>
-  
-        <Modal isOpen={isOpen} onClose={onClose}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader  >WRITE REVIEW</ModalHeader>
-            <ModalCloseButton />
-            <ModalBody w={"100%"} >
-              <Input onChange={(e)=>{setText({...text,name:e.target.value})}} placeholder='Name' />
-              <Textarea onChange={(e)=>{setText({...text,message:e.target.value})}} placeholder='Write Review' />
-             <Rating/>
-            </ModalBody>
-  
-            <ModalFooter>
-              <Button colorScheme='blue' mr={3} onClick={()=>{
-                
-                onClose()
-              }}>
-                Close
-              </Button>
-              <Button variant='ghost'onClick={()=>{
-                if(text.name!=""&&text.message!="")
-                {
+  return (
+    <>
+      <Button onClick={onOpen} bg={mainColor} color="white" fontSize={"10px"}>
+        WRITE REVIEW
+      </Button>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>WRITE REVIEW</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody w={"100%"}>
+            <Input
+              onChange={(e) => {
+                setText({ ...text, name: e.target.value });
+              }}
+              placeholder="Name"
+            />
+            <Textarea
+              onChange={(e) => {
+                setText({ ...text, message: e.target.value });
+              }}
+              placeholder="Write Review"
+            />
+            <Rating />
+          </ModalBody>
+
+          <ModalFooter>
+            <Button
+              colorScheme="blue"
+              mr={3}
+              onClick={() => {
+                onClose();
+              }}
+            >
+              Close
+            </Button>
+            <Button
+              variant="ghost"
+              onClick={() => {
+                if (text.name != "" && text.message != "") {
                   toast({
                     title: `success thank you for your valuable feedback`,
                     status: "success",
                     isClosable: true,
-                  })
-                    onClose()
+                  });
+                  onClose();
+                } else {
+                  toast({
+                    title: `Please fill all inputs`,
+                    status: "info",
+                    isClosable: true,
+                  });
                 }
-             else{
-              toast({
-                title: `Please fill all inputs`,
-                status: "info",
-                isClosable: true,
-              })
-             }
-              }} >Submit</Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-      </>
-    )
-  }
+              }}
+            >
+              Submit
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
